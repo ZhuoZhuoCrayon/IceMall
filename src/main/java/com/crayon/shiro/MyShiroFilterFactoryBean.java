7dab7a4e17c0cde87c03b041c1f90605878232ef
caixiaoxin <873217631@qq.com>
2019-12-12 23:57:35 +0800

    建立[ShiroBean]系列文件

---
+++ src/main/java/com/crayon/shiro/MyShiroFilterFactoryBean.java
@@ -0,0 +1,56 @@
+package com.crayon.shiro;
+
+import com.crayon.pojo.user_manage.Permission;
+import com.crayon.service.impl.user_manage.PermissionServiceImpl;
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.util.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class MyShiroFilterFactoryBean  extends ShiroFilterFactoryBean {
+
+    @Autowired
+    private PermissionServiceImpl permissionService;
+
+
+    private static final Logger log= LoggerFactory.getLogger(MyShiroFilterFactoryBean.class);
+    /**
+     * 配置中的过滤链
+     */
+    public static String definitions;
+    @Override
+    public void setFilterChainDefinitions(String definitions) {
+
+        MyShiroFilterFactoryBean.definitions = definitions;
+
+        List<Permission> permissions = permissionService.findAll();
+        definitions = "/static/** = anon\n" +
+                "/css/** = anon\n" +
+                "/js/** = anon\n" +
+                "/img/** = anon\n" +
+                "/index.jsp = anon\n" +
+                "/404.html = anon\n" +
+                "/test.html = anon\n";
+
+        //导入权限
+        for(Permission permission:permissions){
+            definitions += permission.getPermission() + " = " + permission.getPerms() + "\n";
+        }
+        System.out.println(definitions);
+        log.info(definitions);
+
+        //从配置文件加载权限配置
+        Ini ini = new Ini();
+        ini.load(definitions);
+        Ini.Section section = ini.getSection("urls");
+        if (CollectionUtils.isEmpty(section)) {
+            section = ini.getSection("");
+        }
+
+        this.setFilterChainDefinitionMap(section);
+    }
+}
---
+++ src/main/java/com/crayon/shiro/realm/UserRealm.java
@@ -0,0 +1,94 @@
+package com.crayon.shiro.realm;
+
+import com.crayon.pojo.user_manage.User;
+import com.crayon.service.UserService;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRealm extends AuthorizingRealm {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 权限校验
+     *
+     * @param principals
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        System.out.println("权限校验--执行了doGetAuthorizationInfo...");
+        String username = (String) principals.getPrimaryPrincipal();
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        /*
+         authorizationInfo.setRoles(usersService.findRoles(username));
+        authorizationInfo.setStringPermissions(usersService.findPermissions(username));
+         */
+        //注入用户权限
+        authorizationInfo.setRoles(userService.getRoles(username));
+        authorizationInfo.setStringPermissions(userService.getPermissions(username));
+        return authorizationInfo;
+    }
+
+    /**
+     * 身份校验
+     *
+     * @param token
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        System.out.println("身份校验--执行了goGetAuthenticationInfo...");
+
+        String username = (String) token.getPrincipal();
+
+        User user = userService.findByName(username).get(0);
+
+        if (user == null) {
+            throw new UnknownAccountException(); //没有找到账号
+        }
+        //交给AuthenticationRealm使用CredentialsMatcher进行密码匹配
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                user.getUserName(), //用户名
+                user.getPassword(), //密码
+                ByteSource.Util.bytes(user.getSalt()), //salt=username+salt
+                getName() //realm name
+        );
+        return authenticationInfo;
+    }
+
+    @Override
+    public void clearCachedAuthorizationInfo(PrincipalCollection principals) {
+        super.clearCachedAuthorizationInfo(principals);
+    }
+
+    @Override
+    public void clearCachedAuthenticationInfo(PrincipalCollection principals) {
+        super.clearCachedAuthenticationInfo(principals);
+    }
+
+    @Override
+    public void clearCache(PrincipalCollection principals) {
+        super.clearCache(principals);
+    }
+
+    public void clearAllCachedAuthorizationInfo() {
+        getAuthorizationCache().clear();
+    }
+
+    public void clearAllCachedAuthenticationInfo() {
+        getAuthenticationCache().clear();
+    }
+
+    public void clearAllCache() {
+        clearAllCachedAuthenticationInfo();
+        clearAllCachedAuthorizationInfo();
+    }
+}
