7244dca05d72ed7b2e1a0c39e06b352646248dca
caixiaoxin <873217631@qq.com>
2019-12-13 23:43:22 +0800

    新增[系统控制层]

---
+++ src/main/java/com/crayon/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.crayon.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+}
---
+++ src/main/java/com/crayon/controller/user_manage/SystemController.java
@@ -0,0 +1,86 @@
+package com.crayon.controller.user_manage;
+
+import com.crayon.dto.Result;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+    /**
+     * 用户登录的入口
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public Result login(
+            @RequestParam(value = "username", required = false) String username,
+            @RequestParam(value = "password", required = false) String password,
+            @RequestParam(value = "remember", required = false) String remember) {
+
+        System.out.println("登陆用户输入的用户名：" + username + "，密码：" + password +
+                ", remember me: " + remember);
+        String error = null;
+        if (username != null && password != null) {
+            //初始化
+            Subject subject = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            if (remember != null) {
+                if (remember.equals("true")) {
+                    //说明选择了记住我
+                    token.setRememberMe(true);
+                } else {
+                    token.setRememberMe(false);
+                }
+            } else {
+                token.setRememberMe(false);
+            }
+
+            try {
+                //登录，即身份校验，由通过Spring注入的UserRealm会自动校验输入的用户名和密码在数据库中是否有对应的值
+                subject.login(token);
+                System.out.println("用户是否登录：" + subject.isAuthenticated());
+                //用户获取当前登录的用户名
+                /*String username = (String) SecurityUtils.getSubject().getPrincipal();
+                System.out.println(username);*/
+                return new Result(true, "success");
+            } catch (UnknownAccountException e) {
+                //e.printStackTrace();
+                error = "用户账户不存在";
+            } catch (IncorrectCredentialsException e) {
+                //e.printStackTrace();
+                error = "用户名或密码错误，错误信息";
+            } catch (LockedAccountException e) {
+                //e.printStackTrace();
+                error = "该账号已锁定，错误信息";
+            } catch (DisabledAccountException e) {
+                //e.printStackTrace();
+                error = "该账号已禁用，错误信息";
+            } catch (ExcessiveAttemptsException e) {
+                //e.printStackTrace();
+                error = "该账号登录失败次数过多，错误信息";
+            } catch (Exception e) {
+                //e.printStackTrace();
+                error = "未知错误";
+            }
+        } else {
+            error = "请输入用户名和密码";
+        }
+        //登录失败
+        System.out.println(error);
+        return new Result(false, error);
+    }
+
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    public Result register(){
+        return new Result(false,"");
+    }
+}
