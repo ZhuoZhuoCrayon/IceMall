3123b1b076a3ba4ca25234dc68ab74aff5744da4
caixiaoxin <873217631@qq.com>
2019-12-18 22:56:53 +0800

    Result[新增附加参数(HashMap)，为前端提供功能性参数

--- src/main/java/com/crayon/dto/Result.java
+++ src/main/java/com/crayon/dto/Result.java
@@ -2,15 +2,28 @@ package com.crayon.dto;
 
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 public class Result implements Serializable {
     private Boolean success;
     private String message;
+    private HashMap<String,Object> plusParams;  //存储附加参数
 
+
+    public Result(Boolean success,String message,HashMap<String,Object> plusParams){
+        this.success = success;
+        this.message = message;
+        this.plusParams = plusParams;
+    }
     public Result(Boolean success,String message){
         this.success = success;
         this.message = message;
     }
+
+    public HashMap<String, Object> getPlusParams() {
+        return plusParams;
+    }
+
     public String getMessage() {
         return message;
     }
@@ -25,5 +38,9 @@ public class Result implements Serializable {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setPlusParams(HashMap<String, Object> plusParams) {
+        this.plusParams = plusParams;
     }
 }
