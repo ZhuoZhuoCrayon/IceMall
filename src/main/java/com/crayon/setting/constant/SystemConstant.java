4c8ad5fab00748e9756b35fedd43d215f6e83ec1
caixiaoxin <873217631@qq.com>
2019-12-20 23:39:25 +0800

    新增系统常量区

---
+++ src/main/java/com/crayon/setting/constant/SystemConstant.java
@@ -0,0 +1,7 @@
+package com.crayon.setting.constant;
+
+public interface SystemConstant {
+    String STORE_PATH = "C:/Users/crayon/OneDrive/IceMall/src/main/webapp";
+    String PREVIEW_IMG_PATH = "/static/img/preview/p";
+    String DETAIL_IMG_PATH = "/static/img/detail/";
+}
